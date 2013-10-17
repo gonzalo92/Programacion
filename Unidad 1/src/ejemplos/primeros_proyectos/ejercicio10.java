554bc8c604b551a40dbaf6fc5689e4adaa21f105
gonzalo92 <gonzalogutirrez92@gmail.com>
2013-10-17 17:40:57 +0200

    1

--- Unidad 1/src/ejemplos/primeros_proyectos/ejercicio10.java
+++ Unidad 1/src/ejemplos/primeros_proyectos/ejercicio10.java
@@ -6,6 +6,7 @@ public class ejercicio10 {
 
 	public static void main(String[] args) {
 		//Crea un programa que pida un peso en Kilogramos y realice la conversión a onzas, sabiendo que 1 Kilogramo (Kg) = 35.2739619 Onzas (oz)
+		@SuppressWarnings("resource")
 		Scanner teclado= new Scanner(System.in);
 		float kilogramos;
 		float onzas;
